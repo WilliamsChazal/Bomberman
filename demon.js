3575498b55ae4c18c3f5f26556aff4199491ea80
williams <w.chazal@codeur.online>
2021-07-21 11:56:16 +0200

    tentative colision

--- demon.js
+++ demon.js
@@ -24,17 +24,43 @@ function positionEnemies(enemy){
 function mouvEnemies(){
 
 }
+/*  colisionDetect();
 
-colisionDetect();
-
-function colisionDetect(collision){
+function colisionDetect(enemy){
     let player = document.getElementById('player');
     let enemies = document.getElementsByClassName('enemies');
     let enemiesSize = 150;
     let playerSize = 175;
 
-    if (enemies.offsetTop + enemiesSize == player.offsetTop + playerSize && enemies.offsetLeft + enemiesSize == player.offsetLeft + playerSize) {
-        alert ('colision');
-        } else collision = false
-        console.log(collision);
+    if (enemies.offsetTop == enemiesSize < player.offsetTop + playerSize && enemies.offsetLeft +     enemiesSize == player.offsetLeft + playerSize){
+        alert ('colision');} 
+    else {
+        collision = false}
+    console.log(collision);       
+}  */
+
+function detectCollisions(){
+    let obj1 = document.getElementById('player');
+    let obj2 = document.getElementsByClassName('enemies');
+
+    // Reset collision state of all objects
+    for (let i = 0; i < gameObjects.length; i++) {
+        gameObjects[i].isColliding = false;
+    }
+
+    // Start checking for collisions
+    for (let i = 0; i < gameObjects.length; i++)
+    {
+        obj1 = gameObjects[i];
+        for (let j = i + 1; j < gameObjects.length; j++)
+        {
+            obj2 = gameObjects[j];
+
+            // Compare object1 with object2
+            if (rectIntersect(obj1.x, obj1.y, obj1.width, obj1.height, obj2.x, obj2.y, obj2.width, obj2.height)){
+                obj1.isColliding = true;
+                obj2.isColliding = true;
+            }
+        }
+    }
 }
