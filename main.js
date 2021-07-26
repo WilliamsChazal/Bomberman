69e6205e2e8bf5c4cc427a7e7de7d38d987fc3e9
WilliamsChazal <w.chazal@codeur.online>
2021-07-26 23:13:09 +0200

    pb collision, collision ok/tiers flèches du haut et du bas, code clean

--- main.js
+++ main.js
@@ -74,10 +74,10 @@ function addWall() {
 //Fonction de la mort du doomGuy
 function doomGuyDead() {
   doomGuy.style.backgroundImage="url('assets/doomGuy/mort-1.png')";
-    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-2.png')", 500);
-    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-3.png')", 1000);
-    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-4.png')", 1500);
-    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-5.png')", 2000);
+    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-2.png')", 100);
+    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-3.png')", 300);
+    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-4.png')", 500);
+    setTimeout (()=>doomGuy.style.backgroundImage="url('assets/doomGuy/mort-5.png')", 700);
     /* setTimeout (()=>alert('dead'), 2500); */
 }
 
@@ -90,9 +90,9 @@ document.addEventListener('keydown', function(event) {
     for (let i = 0; i < enemies.length; i++)
       if (colisionDetect (doomGuy,doomGuy.offsetLeft, doomGuy.offsetTop - moveSize, (enemies [i]))) {
           doomGuyDead();}
-      else {for (let y = 0; y < tileset.length; y++)
+/*       else {for (let y = 0; y < tileset.length; y++)
           if (colisionDetect (doomGuy,doomGuy.offsetLeft, doomGuy.offsetTop - moveSize, (tileset [y]))) {
-            console.log('mur');}} 
+            console.log('mur');}}  */
 
       // déplacement
     if(doomGuy.offsetTop >=24){  
@@ -107,9 +107,9 @@ document.addEventListener('keydown', function(event) {
 
   else if (event.code == 'ArrowRight') {
     // essais collision
-    for (let i = 0; i < enemies.length; i++)
-      if (colisionDetect (doomGuy,doomGuy.offsetLeft, doomGuy.offsetTop - moveSize, (enemies [i]))) {
-      doomGuyDead();}
+   /*  for (let i = 0; i < enemies.length; i++)
+      if (colisionDetect (doomGuy,doomGuy.offsetTop, doomGuy.offsetLeft - moveSize, (enemies [i]))) {
+      doomGuyDead();} */
 
     // déplacement  
     if(doomGuy.offsetLeft <=672){
@@ -123,31 +123,35 @@ document.addEventListener('keydown', function(event) {
 } 
 
   else if (event.code == 'ArrowDown') {
-  for (let i = 0; i < enemies.length; i++)
-  if (colisionDetect (doomGuy,doomGuy.offsetLeft, doomGuy.offsetTop - moveSize, (enemies [i]))) {
-    doomGuyDead();
-  }
-  if(doomGuy.offsetTop <= 696){
-    doomGuyWalk = doomGuyWalk + 1;
-    count ++;
-    this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/face-" + count + ".png')";
+    // essais collision
+    for (let i = 0; i < enemies.length; i++)
+    if (colisionDetect (doomGuy,doomGuy.offsetLeft, doomGuy.offsetTop - moveSize, (enemies [i]))) {
+      doomGuyDead();} 
+
+    // déplacement    
+    if(doomGuy.offsetTop <= 696){
+      doomGuyWalk = doomGuyWalk + 1;
+      count ++;
+      this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/face-" + count + ".png')";
     if (count >= 4 ) count=0
-    /* else if (doomGuyWalk % 2 == 0 && count == 1){ this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/face-2.png')";}
-    else if (doomGuyWalk % 2 == 1 && count == 0){ this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/dos-1.png')";} */
+      /* else if (doomGuyWalk % 2 == 0 && count == 1){ this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/face-2.png')";}
+      else if (doomGuyWalk % 2 == 1 && count == 0){ this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/dos-1.png')";} */
     doomGuy.style.top = (doomGuy.offsetTop + moveSize) + "px";
   }
 } 
   else if (event.code == 'ArrowLeft') {
-    for (let i = 0; i < enemies.length; i++)
+    // essais collision
+/*     for (let i = 0; i < enemies.length; i++)
     if (colisionDetect (doomGuy,doomGuy.offsetTop, doomGuy.offsetLeft - moveSize, (enemies [i]))) {
       doomGuyDead();
-    }
+    } */
+    // déplacement  
   if(doomGuy.offsetLeft >= -5){
     doomGuyWalk = doomGuyWalk + 1;
-    // Votre code ici
     if(doomGuyWalk % 2 == 1)
-    this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/gauche-1.png')";
-    else{this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/gauche-2.png')";}
+      this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/gauche-1.png')";
+    else{
+      this.onkeydown = doomGuy.style.backgroundImage="url('assets/doomGuy/gauche-2.png')";}
   doomGuy.style.left = (doomGuy.offsetLeft - moveSize) + "px";
   }
 
